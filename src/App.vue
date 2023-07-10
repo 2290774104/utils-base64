3a22c04a3ddd889d2239b94196ab489d8bbc9fd2
2290774107@qq.com <2290774104@qq.com>
2023-07-10 11:09:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <picture-base64 />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import PictureBase64 from '@/components/picture-base64.vue'
+
+@Options({
+  components: {
+    PictureBase64
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/picture-base64.vue
@@ -0,0 +1,13 @@
+<template>
+  图片转Base64
+  <el-button>测试</el-button>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class PictureBase64 extends Vue {}
+</script>
+
+<style>
+</style>
